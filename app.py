8c173aa6d0af8cf56c214577c2d20292ca410d9f
FizzaFatimaS <fizzafatima545@gmail.com>
2025-05-09 18:17:59 +0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='.')
 
-@app.route("/")
-def serve_html():
+@app.route('/')
+def serve_home():
     return send_from_directory('.', 'index.html')
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000)
